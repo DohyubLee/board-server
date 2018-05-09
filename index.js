451dfc7dbb1afbdbc7efb7e25806abd40995c86b
DohyubLee <hani7304@gmail.com>
2018-05-09 22:11:10 +0900

    80

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ app.post('/comment-delete', (req, res) => {
     })
 })
 
-app.listen(443, function () {
-    console.log('Example app listening on port 443!');
+app.listen(80, function () {
+    console.log('Example app listening on port 80!');
     console.log('press ctrl + c exit');
 });
